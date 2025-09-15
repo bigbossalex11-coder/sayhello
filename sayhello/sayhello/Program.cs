f85e4fe7c9e3c734d30d8c458d97928d2f89a6e2
alex carrizo <maik_alex@hotmail.com>
2025-09-15 11:04:33 +0200

    Add initial project setup for sayhello application
    Created a new Visual Studio solution file (`sayhello.sln`)
    and project file (`sayhello.csproj`) targeting .NET 8.0.
    Added a simple C# program (`Program.cs`) that greets the user.

---
+++ sayhello/sayhello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace sayhello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayhello("World");
+            Console.ReadLine();
+        }
+
+        static void Sayhello(string name)
+        {
+            {
+                Console.WriteLine($"Hello, {name}");
+            }
+        }
+    }
+}
